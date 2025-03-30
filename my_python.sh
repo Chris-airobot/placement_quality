8c14f1f57698f56a32adb6dc63ac4ad8d80a35e4
chris-airobot <tianyuli19981009@gmail.com>
2025-03-30 10:10:45 +0000

    Add a docker file with updating the requirement.txt

--- my_python.sh
+++ my_python.sh
@@ -2,7 +2,7 @@
 while true; do
     echo "Running ycb_collection.py..."
     cd /home/chris/Chris/placement_ws/src/placement_quality
-    ~/.local/share/ov/pkg/isaac-sim-4.2.0/python.sh -m ycb_simulation.ycb_collection --/log/level=error --/log/fileLogLevel=error --/log/outputStreamLevel=error
+    /isaac-sim/python.sh -m ycb_simulation.ycb_collection --/log/level=error --/log/fileLogLevel=error --/log/outputStreamLevel=error
     echo "Script finished. Resting for 1 minute..."
     sleep 60
 done
